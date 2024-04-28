1a7b7efa194b94438cc56129c7ea2e0e758b2732
Mehrdad Jalali <73692912+MehrdadJalali-KIT@users.noreply.github.com>
2024-04-28 19:22:20 +0200

    Add files via upload

---
+++ test game/game 1/game1.m
@@ -0,0 +1,9 @@
+%%NFG 1 R "2x2x2 Example from McKelvey-McLennan, with 9 Nash equilibria, 2 totally mixed" { "Player 1" "Player 2" "Player 3" }
+game1=[ 9, 8, 12;
+ 0, 0, 0 ;
+ 0, 0, 0 ;
+ 9, 8, 2 ;
+ 0, 0, 0 ;
+ 3, 4, 4 ;
+ 3, 4, 6 ;
+ 0, 0, 0 ; ]
